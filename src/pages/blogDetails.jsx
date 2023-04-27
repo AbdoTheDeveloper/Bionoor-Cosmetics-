77598fc02cf52d791d282f72d07af4429f348002
AbdoTheDeveloper <akasbaji2@gmail.com>
2023-04-27 01:26:15 +0100

    This Is the first commit

---
+++ src/pages/blogDetails.jsx
@@ -0,0 +1,120 @@
+const blogDetails = () => {
+    return ( 
+        <div className="blog_details_container">
+            <main className="blog_details_card">
+                <img src="" alt="" />
+                <span id = 'categorie'>
+
+                </span>
+                <p id = 'content'>
+
+                </p>
+
+
+            </main>
+            <aside>
+                <div>
+                    <input type="search" />
+                    <span className="search_icon">
+
+                    </span>
+                </div>
+                <div className="recent-pots">
+                    <h5></h5>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className="categories">
+                    <h5></h5>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className="archives">
+                <h5></h5>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </aside>
+
+                <h5></h5>
+                <div id='rating_stars'>
+                    <i></i>
+                    <i></i>
+            <div id="rate_blog">
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                </div>
+
+            </div>
+            <section className="comments-section">
+            <div className="comments">
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div className="comment">
+                    <h6></h6>
+                    <p></p>
+                    <span></span>
+                </div>
+            </div>
+            <div className="post_comment">
+                <p></p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <input type="text" /><input type="text" />
+                <input type="checkbox" name="" id="" />
+                <p></p>
+                <button id="post_comment_button"></button>
+            </div>
+            </section>
+        </div>
+     );
+}
+ 
+export default blogDetails;
